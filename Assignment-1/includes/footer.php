f20e3105ac3f450f7447c49267113e7836f4803b
Mitchell <mitchellmwf@gmail.com>
2025-11-01 01:50:47 +0000

    Removed php cause github pages cant host it.

--- Assignment-1/includes/footer.php
+++
@@ -1,5 +0,0 @@
-<!--Small footer with legal notice and some (empty)links -->
-<footer>
-    <p><small>© Mario's Pizza Palace. All rights reserved.</small></p>
-    <p><a href="">Pivacy policy</a> <a href="">About</a> <a href="">Instagram</a></p>
-</footer>
